1b80b9eac883eb11e215f7461c6db444e4643c0f
Madhu <madhuthakur2321998@gmail.com>
2026-02-18 13:16:09 +0530

    adding loading in application

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,51 +2,54 @@ import React, { useState } from "react";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-//   function fetchMoviesHandler() {
-//     fetch("https://swapi.dev/api/films/")
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((data) => {
-//         const transformedMovies = data.results.map((movieData) => {
-//           return {
-//             id: movieData.episode_id,
-//             title: movieData.title,
-//           };
-//         });
+  //   function fetchMoviesHandler() {
+  //     fetch("https://swapi.dev/api/films/")
+  //       .then((response) => {
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         const transformedMovies = data.results.map((movieData) => {
+  //           return {
+  //             id: movieData.episode_id,
+  //             title: movieData.title,
+  //           };
+  //         });
 
-//         setMovies(transformedMovies);
-//       });
-//   }
+  //         setMovies(transformedMovies);
+  //       });
+  //   }
 
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
 
-async function fetchMoviesHandler() {
-  const response = await fetch("https://swapi.dev/api/films/");
+    const response = await fetch("https://swapi.dev/api/films/");
 
-  const data = await response.json();
+    const data = await response.json();
 
-  const transformedMovies = data.results.map((movieData) => {
-    return {
-      id: movieData.episode_id,
-      title: movieData.title,
-    };
-  });
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+      };
+    });
 
-  setMovies(transformedMovies);
-}
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
 
   return (
     <div>
-      <button onClick={fetchMoviesHandler}>
-        Fetch Movies
-      </button>
-
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
